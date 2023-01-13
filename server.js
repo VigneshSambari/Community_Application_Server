a8f5dcf177cbb513afa9c5b8ec2f6f2a37f19aef
VickySam1901 <72963230+VickySam1901@users.noreply.github.com>
2023-01-13 14:15:30 +0530

    commit0

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 5000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("hoila");
+});
+
+const Port=process.env.PORT||2000;
+
+app.listen(Port,"0.0.0.0",()=>console.log("Running"));
