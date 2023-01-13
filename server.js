113e3780a7af57c2c73a15ad39711e6692d531fe
NuthanReddy45 <sainuthanreddy264@gmail.com>
2023-01-13 20:31:32 +0530

    Db setUp

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const connectDB = require("./database/db");
 const app = express();
 
 const PORT = 5000 || process.env.PORT;
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("hoila");
