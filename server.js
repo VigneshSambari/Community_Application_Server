2811dbfd8a7b46c80c3d01c26db8723425be7963
NuthanReddy45 <sainuthanreddy264@gmail.com>
2023-01-13 20:34:29 +0530

    Db setUp

--- server.js
+++ server.js
@@ -10,6 +10,4 @@ app.get("/", (req, res) => {
   res.send("hoila");
 });
 
-const Port=process.env.PORT||2000;
-
-app.listen(Port,"0.0.0.0",()=>console.log("Running"));
+app.listen(Port, "0.0.0.0", () => console.log("Running"));
