5d6352cd590509262c31508a268ceec0191a5a2c
VickySam1901 <72963230+VickySam1901@users.noreply.github.com>
2023-02-04 19:48:56 +0530

    renamed file

--- models/college.model.js
+++ models/college.model.js
@@ -10,7 +10,7 @@ const CollegeSchema = new mongoose.Schema(
             {
                 type: mongoose.Schema.Types.ObjectId,
                 ref: "Room",
-            }
+            } 
         ]
     }
 )
